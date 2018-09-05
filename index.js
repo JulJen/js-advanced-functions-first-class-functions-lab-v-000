f056ebb005f002ca9e234f3528462169b2355016
Jul Jen <juliajen.prog@gmail.com>
2018-09-05 16:18:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ const fareTripler = createFareMultiplier(3);
 
 // This function takes two arguments, an array of drivers and either the returnFirstTwoDrivers() or returnLastTwoDrivers() function. Based on these two arguments, selectDifferentDrivers() will return either the first two drivers or the last two drivers.
 
-function selectDifferentDrivers(drivers, callback) {
-  return callback(drivers);
+function selectDifferentDrivers(drivers, selectingDrivers) {
+  return selectingDrivers(drivers);
 }
